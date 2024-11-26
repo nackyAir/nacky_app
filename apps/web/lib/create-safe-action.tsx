76630a1b8e399cf21751fdc5f2b51d94388997cf
nackyAir <hmcn3176naoki8086@gmail.com>
2024-11-26 15:03:26 +0900

    fea: create-safe-actionの作成。

---
+++ apps/web/lib/create-safe-action.tsx
@@ -0,0 +1,66 @@
+import {
+  createSafeActionClient,
+  flattenValidationErrors,
+} from 'next-safe-action'
+import { z } from 'zod'
+
+export type FieldErrors<T> = {
+  [key in keyof T]?: string[]
+}
+
+export type ActionsState<TInput, TOutput> = {
+  fieldErrors?: FieldErrors<TInput>
+  errors?: string | null
+  data?: TOutput
+}
+
+const metaDataSchema = z.object({
+  actionName: z.string(),
+})
+
+class ActionError extends Error {
+  metaData: z.infer<typeof metaDataSchema>
+
+  constructor(message: string, metaData: z.infer<typeof metaDataSchema>) {
+    super(message)
+    this.name = 'ActionError'
+    this.metaData = metaData
+  }
+}
+
+export function useSafeAction<TInput, TOutput>(
+  actionName: string,
+  schema: z.Schema<TInput>,
+  handler: (input: TInput) => Promise<ActionsState<TInput, TOutput>>
+) {
+  return createSafeActionClient({
+    handleServerError: (e) => {
+      if (e instanceof ActionError) {
+        return {
+          message: e.message,
+          ...e.metaData,
+        }
+      }
+
+      return {
+        message: e.message || '予期せぬエラーが発生しました',
+        actionName,
+      }
+    },
+
+    defineMetadataSchema() {
+      return metaDataSchema
+    },
+  })
+    .metadata({
+      actionName,
+    })
+    .schema(schema, {
+      handleValidationErrorsShape: (ve) =>
+        flattenValidationErrors(ve).fieldErrors as FieldErrors<TInput>,
+    })
+
+    .action(async ({ parsedInput }) => {
+      return handler(parsedInput)
+    })
+}
