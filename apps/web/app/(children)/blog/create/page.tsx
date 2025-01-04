3091e6b8af45d5a80d037fc1179abd040de4615b
nackyAir <hmcn3176naoki8086@gmail.com>
2025-01-05 00:53:29 +0900

    fix:importの修正。

--- apps/web/app/(children)/blog/create/page.tsx
+++ apps/web/app/(children)/blog/create/page.tsx
@@ -1,8 +1,5 @@
 import { Suspense } from 'react'
 
-import { RichTextEditor } from '@repo/ui/components/RichTextEditor'
-import { Form } from '@repo/ui/components/form'
-
 import { BlogCreateWrapper } from '~/features/Blog/BlogCreateWrapper'
 import { LoadingScreen } from '~/features/Layout'
 
